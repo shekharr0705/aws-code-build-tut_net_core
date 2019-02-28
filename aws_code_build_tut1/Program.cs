badbf558a28955736a8afd0791a475710e1c1194
Shekhar Rajepandhare <shekharr@tavisca.com>
2019-02-28 11:14:05 +0530

    first commit

---
+++ aws_code_build_tut1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace aws_code_build_tut1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World.......!!!!");
+            Thread.Sleep(5000);
+        }
+    }
+}
